cc841e1145c194a9ee430795509e7bf02327bba7
Jason Tsukahara <tsuky19@gmail.com>
2019-09-13 13:43:54 -0400

    fix dplyr

--- R/sem_descriptives.R
+++ R/sem_descriptives.R
@@ -10,14 +10,14 @@ sem_descriptives <- function(x){
   x <- tidyr::gather(x, "Variable", "value")
   x <- dplyr::group_by(x, Variable)
   x <- dplyr::summarise(x,
-                        n = n(),
+                        n = dplyr::n(),
                         Mean = mean(value, na.rm=TRUE),
                         SD = sd(value, na.rm=TRUE),
                         min = min(value, na.rm=TRUE),
                         max = max(value, na.rm=TRUE),
                         Skewness = e1071::skewness(value, na.rm = TRUE, type = 2),
                         Kurtosis = e1071::kurtosis(value, na.rm = TRUE, type = 1),
-                        '% Missing' = 100*(length(which(is.na(value)))/n()))
+                        '% Missing' = 100*(length(which(is.na(value)))/dplyr::n()))
   x <- knitr::kable(x, digits=2, format="html", caption="Descriptive Statistics")
   x <- kableExtra::kable_styling(x)
   return(x)
