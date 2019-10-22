bf04547a3f8ac772ff9b0ab5d2a3b6a4444edd26
dr-JT <tsuky19@gmail.com>
2019-10-22 14:54:33 -0400

    update n to include only non-missing values

--- R/sem_descriptives.R
+++ R/sem_descriptives.R
@@ -3,22 +3,31 @@
 #' This function will display basic descriptive statistics for a dataframe
 #' @param x dataframe
 #' @export
-#' @examples
-#' sem_descriptives(x)
+#'
 
 sem_descriptives <- function(x){
   x <- tidyr::gather(x, "Variable", "value")
   x <- dplyr::group_by(x, Variable)
-  x <- dplyr::summarise(x,
-                        n = dplyr::n(),
-                        Mean = mean(value, na.rm=TRUE),
-                        SD = sd(value, na.rm=TRUE),
-                        min = min(value, na.rm=TRUE),
-                        max = max(value, na.rm=TRUE),
-                        Skewness = e1071::skewness(value, na.rm = TRUE, type = 2),
-                        Kurtosis = e1071::kurtosis(value, na.rm = TRUE, type = 1),
-                        '% Missing' = 100*(length(which(is.na(value)))/dplyr::n()))
-  x <- knitr::kable(x, digits=2, format="html", caption="Descriptive Statistics")
-  x <- kableExtra::kable_styling(x)
-  return(x)
+  table <- dplyr::summarise(x,
+                            n = length(which(!is.na(value))),
+                            Mean = mean(value, na.rm=TRUE),
+                            SD = sd(value, na.rm=TRUE),
+                            min = min(value, na.rm=TRUE),
+                            max = max(value, na.rm=TRUE),
+                            Skewness =
+                              e1071::skewness(value, na.rm = TRUE, type = 2),
+                            Kurtosis =
+                              e1071::kurtosis(value, na.rm = TRUE, type = 2),
+                            '% Missing' =
+                              100 * (length(which(is.na(value))) / dplyr::n()))
+
+  N <- dplyr::summarise(x, N.total = dplyr::n())
+  N <- N$N.total[1]
+  table <- dplyr::ungroup(table)
+  table <- knitr::kable(table, digits=2, format="html",
+                        caption="Descriptive Statistics")
+  table <- kableExtra::kable_styling(table)
+  table <- kableExtra::footnote(table, general_title = "\n",
+                                general = paste("Total N = ", N, sep = ""))
+  return(table)
 }
