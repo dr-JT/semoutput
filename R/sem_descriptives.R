7410a89bfe9d433e005c357d2a031e2abe685a93
Tsukahara <jtsukahara3@psy-351-01.psych.gatech.edu>
2020-02-21 17:11:10 -0500

    delete deprecated functions

--- R/sem_descriptives.R
+++ R/sem_descriptives.R
@@ -3,6 +3,27 @@
 #' This function will display basic descriptive statistics for a dataframe
 #' @param x dataframe
 #' @export
+#' @example
+#' library(lavaan)
+#'
+#' model <- '
+#' # measurement model
+#' ind60 =~ x1 + x2 + x3
+#' dem60 =~ y1 + y2 + y3 + y4
+#' dem65 =~ y5 + y6 + y7 + y8
+#' # regressions
+#' dem60 ~ ind60
+#' dem65 ~ ind60 + dem60
+#' # residual correlations
+#' y1 ~~ y5
+#' y2 ~~ y4 + y6
+#' y3 ~~ y7
+#' y4 ~~ y8
+#' y6 ~~ y8
+#' '
+#' fit <- sem(model, data = PoliticalDemocracy)
+#'
+#' sem_descriptives(fit)
 #'
 
 sem_descriptives <- function(x){
