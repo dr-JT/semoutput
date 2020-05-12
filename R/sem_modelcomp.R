7b7afcb9b9ad8a1a4745c96dab28119bbc2e8f83
Jason Tsukahara <tsuky19@gmail.com>
2020-05-12 17:01:33 -0400

    update

--- R/sem_modelcomp.R
+++ R/sem_modelcomp.R
@@ -1,8 +1,8 @@
 #' Model comparison
 #'
 #' This function will display a table of Model significance tests
-#' @param m1 A nested lavaan model
-#' @param m2 A nested lavaan model
+#' @param m1 A nested lavaan model. Null hypothesis
+#' @param m2 A nested lavaan model. Alternative hypothesis
 #' @param print Create a knitr table for displaying as html table (default = TRUE)
 #' @param method Model comparison method. "chi-square" or "BIC"
 #' @export
@@ -22,7 +22,7 @@ sem_modelcomp <- function(m1, m2, print = TRUE, method = "chi-square"){
     df2 <- lavaan::fitMeasures(m2, c("df"))[[1]]
     bic1 <- BIC(m1)
     bic2 <- BIC(m2)
-    bf <- exp((bic1 - bic2) / 2)
+    bf <- exp((bic2 - bic1) / 2)
 
     table <- data.frame(Model = c(1, 2), df = c(df1, df2),
                         BIC = c(bic1, bic2), BF = c(NA, bf))
