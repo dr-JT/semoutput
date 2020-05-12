84b16acfa40515772260b50295ebc792bfd5a455
Jason Tsukahara <tsuky19@gmail.com>
2020-05-12 15:05:40 -0400

    add bayesfactor model comparison

--- R/sem_modelcomp.R
+++ R/sem_modelcomp.R
@@ -4,14 +4,28 @@
 #' @param m1 A nested lavaan model
 #' @param m2 A nested lavaan model
 #' @param print Create a knitr table for displaying as html table (default = TRUE)
+#' @param method Model comparison method. "chi-square" or "BIC"
 #' @export
 #'
 
-sem_modelcomp <- function(m1, m2, print = TRUE){
-  stats <- lavaan::anova(m1, m2)
-  table <- suppressWarnings(broom::tidy(stats))
-  table <- dplyr::arrange(table, desc(df))
-  table$term <- c(1, 2)
+sem_modelcomp <- function(m1, m2, print = TRUE, method = "chi-square"){
+  if (method = "chi-square") {
+    stats <- lavaan::anova(m1, m2)
+    table <- suppressWarnings(broom::tidy(stats))
+    table <- dplyr::arrange(table, desc(df))
+    table$term <- c(1, 2)
+  }
+
+  if (method = "BIC") {
+    df1 <- lavaan::fitMeasures(m1, c("df"))
+    df2 <- lavaan::fitMeasures(m2, c("df"))
+    bic1 <- BIC(m1)
+    bic2 <- BIC(m2)
+    bf <- exp((bic1 - bic2)/2)
+
+    table <- data.frame(Model = c(1, 2), df = c(df1, df2),
+                        BIC = c(bic1, bic2), BF = c(NA, bf))
+  }
 
   if (print == TRUE){
     table <- knitr::kable(table, digits = 3, format = "html",
