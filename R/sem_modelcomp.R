866480c9e664bb60ccdec5887acd6d99bfeab7f6
Jason Tsukahara <tsuky19@gmail.com>
2020-05-13 20:43:44 -0400

    update

--- R/sem_modelcomp.R
+++ R/sem_modelcomp.R
@@ -15,16 +15,21 @@ sem_modelcomp <- function(m1, m2, print = TRUE){
   bf <- exp((bic2 - bic1) / 2)
 
   table <- suppressWarnings(broom::tidy(stats))
-  table <- dplyr::rename(Model = term,
+  table <- dplyr::rename(table, Model = term,
                          `Chi Square` = statistic)
-  table$Model <- c(1, 2)
   table <- dplyr::mutate(table,
+                         Model = ifelse(Model == "m1", 1, 2))
+  table <- dplyr::arrange(table, Model)
+  table <- dplyr::mutate(table,
+                         Chisq.diff = ifelse(Model == 2,
+                                             dplyr::first(Chisq.diff), NA),
+                         df.diff = ifelse(Model ==2,
+                                          dplyr::first(Df.diff), NA),
+                         p = ifelse(Model == 2,
+                                    dplyr::first(p.value), NA),
                          BICnull = dplyr::first(BIC),
                          `Bayes Factor` = ifelse(Model == 1, NA,
                                                  exp((BIC - BICnull) / 2)))
-  table <- dplyr::select(Model, df, AIC, BIC, `Bayes Factor`,
-                         `Chi Square`, Chisq.diff, df.diff = Df.diff,
-                         p = p.value)
 
   if (print == TRUE){
     table <- knitr::kable(table, digits = 3, format = "html",
