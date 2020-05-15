69c134fca7fe5b627f30c0f35e956c45a80414d4
Jason Tsukahara <tsuky19@gmail.com>
2020-05-14 23:11:55 -0400

    update

--- R/sem_modelcomp.R
+++ R/sem_modelcomp.R
@@ -1,24 +1,25 @@
 #' Model comparison
 #'
 #' This function will display a table of Model significance tests
-#' @param m1 A nested lavaan model. Null hypothesis
-#' @param m2 A nested lavaan model. Alternative hypothesis
+#' @param m0 A nested lavaan model. Null hypothesis
+#' @param m1 A nested lavaan model. Alternative hypothesis
 #' @param print Create a knitr table for displaying as html table (default = TRUE)
 #' @export
 #'
 
-sem_modelcomp <- function(m1, m2, print = TRUE){
+sem_modelcomp <- function(m0, m1, print = TRUE){
 
-  stats <- lavaan::anova(m1, m2)
-  bic1 <- BIC(m1)
-  bic2 <- BIC(m2)
+  stats <- lavaan::anova(m0, m1)
+  bic1 <- BIC(m0)
+  bic2 <- BIC(m1)
   bf <- exp((bic2 - bic1) / 2)
 
   table <- suppressWarnings(broom::tidy(stats))
   table <- dplyr::rename(table, Model = term,
                          `Chi Square` = statistic)
   table <- dplyr::mutate(table,
-                         Model = ifelse(Model == "m1", 1, 2))
+                         Model = ifelse(Model == "m0",
+                                        "0 (Null)", "1 (Alternative)"))
   table <- dplyr::arrange(table, Model)
   table <- dplyr::mutate(table,
                          Chisq.diff = ifelse(Model == 2,
@@ -28,12 +29,12 @@ sem_modelcomp <- function(m1, m2, print = TRUE){
                          p = ifelse(Model == 2,
                                     dplyr::first(p.value), NA),
                          BICnull = dplyr::first(BIC),
-                         BF.01 = ifelse(Model == 1, NA,
-                                                 exp((BIC - BICnull) / 2)),
-                         `P(Model|Data)` = last(BF.01) / (last(BF.01) + 1),
+                         BF.10 = ifelse(Model == 1, NA,
+                                                 exp((BICnull - BIC) / 2)),
+                         `P(Model|Data)` = last(BF.10) / (last(BF.10) + 1),
                          `P(Model|Data)` = ifelse(Model == 1, `P(Model|Data)`,
                                                   1 - `P(Model|Data)`))
-  table <- dplyr::select(table, Model, df, AIC, BIC, BF.01, `P(Model|Data)`,
+  table <- dplyr::select(table, Model, df, AIC, BIC, BF.10, `P(Model|Data)`,
                          `Chi Square`, `Chi Square Diff` = Chisq.diff,
                          `df Diff` = df.diff, p)
 
