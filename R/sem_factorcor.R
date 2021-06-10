77e1476c855427c6d458437df3b9026f39775953
Jason Tsukahara <jason.tsukahara@gatech.edu>
2021-06-09 22:04:20 -0400

    automatically detect factor labels

--- R/sem_factorcor.R
+++ R/sem_factorcor.R
@@ -2,12 +2,13 @@
 #'
 #' This function will display a table of Latent factor correlations
 #' @param x a cfa() or sem() lavaan model
-#' @param factors list c() of factors included in the model
+#' @param factors depricated.
 #' @param print Create a knitr table for displaying as html table (default = TRUE)
 #' @export
 #'
 
 sem_factorcor <- function(x, factors = c(), print = TRUE){
+  factors <- x@pta$vnames$lv[[1]]
   table <- lavaan::standardizedSolution(x, level = 0.95)
   table <- dplyr::filter(table,
                          op == "~~",
@@ -24,7 +25,8 @@ sem_factorcor <- function(x, factors = c(), print = TRUE){
 
   if (print == TRUE){
     table <- knitr::kable(table, digits = 3, format = "html",
-                          caption = "Latent Factor Correlations")
+                          caption = "Latent Factor Correlations",
+                          row.names = FALSE)
     table <- kableExtra::kable_styling(table, full_width = FALSE,
                                        position = "left")
   } else if (print == FALSE){
