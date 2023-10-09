445f0ecc12c894ca0c492f09332cf976db7ac5e3
Jason Tsukahara <jtsukahara3@gatech.edu>
2023-10-08 22:57:49 -0400

    update to gt tables

--- R/sem_tables.R
+++ R/sem_tables.R
@@ -4,43 +4,44 @@
 #' specified in the R code chunk setting.
 #'
 #' @param x a lavaan model
-#' @param standardized logical. Include standardized loadings? (default = TRUE)
-#' @param ci logical. display standardized or unstandardized confidence
-#'     intervals? (default = "standardized"). Not needed if standardized=FALSE
+#' @param standardized Logical, indicating whether or not to print standardized
+#'      estimates. Standardized estimates are based on "refit" of the model
+#'      on standardized data but it will not standardize categorical predictors.
+#'      Defualt is TRUE.
+#' @param unstandardized Logical, indicating whether or not to print
+#'      unstandardized estimates. Default is TRUE.
 #' @param ci_level What level of confidence interval to use (default = 0.95)
 #' @param digits How many digits to display? (default = 3)
 #' @export
 #'
 
-sem_tables <- function(x, standardized = TRUE, ci = "standardized",
-                       ci_level = 0.95, digits = 3){
-  # Function to make sure table prints whether running code in R code chunk,
-  # with knit button or with rmarkdown::render()
-  print_table <- function(x) {
-    if (isTRUE(getOption('knitr.in.progress'))) {
-      writeLines(x)
-    } else {
-      print(x)
-    }
-  }
+sem_tables <- function(x, standardized = TRUE, unstandardized = FALSE,
+                       ci_level = 0.95, digits = 3) {
 
-  sig_table <- sem_sig(x, print = TRUE)
-  fit_table <- sem_fitmeasures(x, print = TRUE)
-  loadings_table <- sem_factorloadings(x, standardized = standardized, ci = ci,
-                                       ci_level = ci_level, digits = digits,
-                                       print = TRUE)
-  paths_table <- sem_paths(x, standardized = standardized, ci = ci,
-                           ci_level = ci_level, digits = digits,
-                           print = TRUE)
-  corr_table <- sem_factorcor(x, print = TRUE)
-  var_table <- sem_factorvar(x, print = TRUE)
-  rsquared_table <- sem_rsquared(x, print = TRUE)
+  sig_table <- sem_sig(x, digits = digits)
+  table <- gt::gt_group(sig_table)
 
-  if (sig_table != "") print_table(sig_table)
-  if (fit_table != "") print_table(fit_table)
-  if (loadings_table != "") print_table(loadings_table)
-  if (paths_table != "") print_table(paths_table)
-  if (corr_table != "") print_table(corr_table)
-  if (var_table != "") print_table(var_table)
-  if (rsquared_table != "") print_table(rsquared_table)
+  fit_table <- sem_fitmeasures(x, ci_level = ci_level, digits = digits)
+  if (fit_table != "") table <- gt::grp_add(table, fit_table)
+
+  loadings_table <- sem_factorloadings(x, standardized = standardized,
+                                       unstandardized = unstandardized,
+                                       ci_level = ci_level, digits = digits)
+  if (loadings_table != "") table <- gt::grp_add(table, loadings_table)
+
+  paths_table <- sem_paths(x, standardized = standardized,
+                           unstandardized = unstandardized,
+                           ci_level = ci_level, digits = digits)
+  if (paths_table != "") table <- gt::grp_add(table, paths_table)
+
+  corr_table <- sem_factorcor(x, ci_level = ci_level, digits = digits)
+  if (corr_table != "") table <- gt::grp_add(table, corr_table)
+
+  var_table <- sem_factorvar(x, digits = digits)
+  if (var_table != "") table <- gt::grp_add(table, var_table)
+
+  rsquared_table <- sem_rsquared(x, digits = digits)
+  if (rsquared_table != "") table <- gt::grp_add(table, rsquared_table)
+
+  return(table)
 }
